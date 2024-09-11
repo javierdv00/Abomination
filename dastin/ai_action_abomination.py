d06610d3c186ee0132f7110ace76c35a759cbe43
StudentDastin <163409328+StudentDastin@users.noreply.github.com>
2024-09-11 06:24:42 +0200

    Update ai_action_abomination.py

--- dastin/ai_action_abomination.py
+++ dastin/ai_action_abomination.py
@@ -1,13 +1,25 @@
 from openai import OpenAI
 import json
+from db_for_ai.py import conectorDatabase
+import requests
+from PIL import Image
+from io import BytesIO
+import os
 
 with open('setings.json','r') as file:
     data = json.load(file)
 print(data["API-Key"])
 
-class Oaica2():
-    def __init__(self, ) -> None:
-        self.system_prompt = []
+class Oaica2(conectorDatabase):
+    def __init__(self, image_name="image") -> None:
+        super().__init__()
+        curs, conn = self.conect_to_database("userki33")
+        query = "SELECT prompt FROM KI_Role;"
+        curs.execute(query)
+        self.system_prompt = [prompt[0] for prompt in curs.fetchall()]
+        for prompt in self.system_prompt:
+            print(f"Prompt:{prompt}")
+        """
         # Jornalist for wrting text about Football
         self.system_prompt.append("You are an AI assistant specializing in writing articles about football. You create informative, engaging, and well-researched texts about teams, players, coaches, tactics, and current events in football. Make sure to write clearly, precisely, and in an entertaining manner.")
         # For Chating with a Footballplayer
@@ -16,18 +28,19 @@ class Oaica2():
         self.system_prompt.append("You are a football (soccer) fan. You enjoy talking about football. Remember, you are just a fan and not a professional, so you sometimes express partial knowledge. You can be a know-it-all.")
         # for chatting with a Football hater
         self.system_prompt.append("You don't like football (soccer). You speak negatively about it. You easily get emotional in discussions and conversations. When responding, you tend to write slightly longer texts.")
+        """
+        self.image_name = image_name
 
-    
-    # message:output.append({"role": "system", message})
-    # decision_maker for right role
-    def main (self, message, decision_maker):
-        output = []
-        output.append({"role": "system", "content": self.system_prompt[decision_maker]})
-        output.append({"role": "user", "content": message})
-        return self.get_response(messages=output)
-        
-    def get_response(self, messages) -> str:
+    # decision_maker for right role       
+    def get_text_response(self, message, decision_maker=None) -> str:
         global data
+
+        messages = []
+        messages.append({"role": "system", "content": self.system_prompt[decision_maker]})
+        messages.append({"role": "user", "content": message})
+
+        #return self.get_text_response(messages=output)
+
         client = OpenAI(api_key=data["API-Key"]) 
         response = client.chat.completions.create(
         model="gpt-4o",  # set model here
@@ -38,13 +51,58 @@ class Oaica2():
         #print(response)
         return response.choices[0].message.content
 
+    def get_image_response(self, description_for_image, how_many_image=1, how_many_try=None, ignore_all=None):
+        global data
+        if how_many_try == None:
+            how_many_try = how_many_image * 2
+            if how_many_try < 5:
+                how_many_try = 5
+        client = OpenAI(api_key=data["API-Key"])        
+        if not os.path.exists("image_number.json"):
+            with open("image_number.json", 'w') as file:
+                json.dump( {"number": 0}, file, indent=4) 
+        with open('image_number.json','r') as file:
+            data1 = json.load(file)             
+        index = data1["number"] +1
+        how_many_image = index + how_many_image
+        fail = 0
+        image_list = []
+        while (index < how_many_image) or ((fail + index) < how_many_try):            
+            try:
+                response = client.images.generate(
+                    model="dall-e-3",
+                    prompt= description_for_image,
+                    size = "1792x1024",
+                    quality="hd",
+                    n=1
+                )
+            except Exception as e:
+                response = None
+                print(f"Fail:{e}")
+                fail = fail + 1
+            if response:
+                image_url = response.data[0].url
+                response = requests.get(image_url)
+                image = Image.open(BytesIO(response.content))
+                image_path = f"pictureF{index}.png"  # Hier kannst du den gewünschten Pfad und Dateinamen angeben
+                image.save(image_path)
+
+                data1["number"] = index
+                with open('image_number.json', 'w') as file:
+                    json.dump(data1, file, indent=4)
+                index = index + 1
+                image_list.append(image_path)
+        return image_list
+
+
 # only for testing 
 
-def main():
+def testing_example():
     obj = Oaica2()
-    answer = obj.main(message="Write a articel with 200 Words about Leoni Messi", decision_maker=0)
+    answer = obj.get_text_response(message="Write a articel with 200 Words about Leoni Messi", decision_maker=0)
     print(answer)
     with open("answer.json", "w") as json_file:
         json.dump(answer, json_file)
+    obj.get_image_response("A dynamic shot of Lionel Messi, dribbling past defenders with effortless grace. His intense focus and exceptional control highlight his mastery of the game. The background shows a packed stadium, capturing the excitement and admiration of fans witnessing his brilliance on the pitch.", how_many_image=4, how_many_try=12)
 
-main()
+ #   def get_image_response(self, description_for_image, how_many_image=1, how_many_try=None, ignore_all=None):
