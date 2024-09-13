29c6a6322b6e14321f3296cc4bba4cbf884c5a34
StudentDastin <163409328+StudentDastin@users.noreply.github.com>
2024-09-13 16:13:26 +0200

    Create test_db_for_ai.py

---
+++ dastin/Beta/test_db_for_ai.py
@@ -0,0 +1,17 @@
+import unittest
+from db_for_ai import * 
+
+class TestBuilderDatabsae(unittest.TestCase):
+        pass
+class TestViewDatabase(unittest.TestCase):
+        pass
+class TestDataDatabse(unittest.TestCase):
+        pass
+class TestQueryDatabase(unittest.TestCase):
+        pass
+class TestConectorDatabase(unittest.TestCase):
+        pass
+class TestManagerDatabase(unittest.TestCase):
+        pass
+if __name__ == "__main__":
+    pass
