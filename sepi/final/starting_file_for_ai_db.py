340263fbe7b7672c31486eba3d9e53e3a6b0c73d
StudentDastin <163409328+StudentDastin@users.noreply.github.com>
2024-09-25 06:33:31 +0200

    Create starting_file_for_ai_db.py

---
+++ sepi/final/starting_file_for_ai_db.py
@@ -0,0 +1,59 @@
+from db_for_ai import databaseModel
+
+# Jornalist for wrting text about Football
+promtJornal = """You are an AI assistant specializing in writing 
+articles about football. You create informative,
+ engaging, and well-researched texts about teams, 
+ players, coaches, tactics, and current events in football. 
+ Make sure to write clearly, precisely, and in an entertaining manner.
+ The output happens in the format of the JSON listed at the end. 
+ The Title goes into the Title field and the actual text of the article goes into the Body field.
+ {
+    "Title": "Sample Title",
+    "Body": "This is the body of the document. It contains the main content."
+}
+ """
+
+        # For Chating with a Footballplayer
+promtFootballPlayer = """You are a football (soccer) player. 
+You enjoy writing about football with others. You participate
+ in football discussions with a lot of passion."""
+
+        # for chatting with a other footballfan
+promtFootballFan = """You are a football (soccer) fan. You enjoy 
+talking about football. Remember, you are just a fan and not a 
+professional, so you sometimes express partial knowledge. You can 
+be a know-it-all."""
+        # for chatting with a Football hater
+promtFootballHater = """You don't like football (soccer). You speak
+ negatively about it. You easily get emotional in discussions and 
+ conversations. When responding, you tend to write slightly longer texts."""
+
+promtImageFinder = """Your task is to create a description for an image
+ that fits an article. The image should reflect the core point of the article.
+   Describe the image as precisely as possible. Maximum 50 words"""
+
+list_of_KI_query = [promtJornal, promtFootballPlayer, promtFootballFan, promtFootballHater, promtImageFinder]
+list_of_name = ["Jornal", "FootballPlaye4r", "FootballFan", "Fooballhater", "ImageFinder"]
+
+database_obj = databaseModel("userki910") #43
+
+database_obj.create_database()
+database_obj.create_table()
+database_obj.insert_to_Database("User_", ["name", "rank"], ["standard_user", 1])
+
+index = 0
+for query in list_of_KI_query:     
+    database_obj.insert_to_Database("KI_Role", ["Name", "prompt"], [list_of_name[index], query])
+    index = index + 1
+
+database_obj.create_view()
+
+database_obj.insert_to_Database("User_", ["ID", "name", "rank"], [2, "User", 1]) # a start user
+
+def testing():
+    cur, con = database_obj.conect_to_database(database_obj.database_name)
+
+
+# Ergebnisse abrufen
+testing()
